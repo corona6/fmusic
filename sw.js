d834d5e2cfe629dad59674e9289a71484f7e26c8
corone <fxfx_pot@yahoo.co.jp>
2025-01-31 00:43:35 +0900

    first commit

---
+++ sw.js
@@ -0,0 +1,78 @@
+
+const MAIN_CACHE = 'main_20250131';
+
+self.addEventListener("install", async (event) => {
+    event.waitUntil((async () => {
+        const cache = await caches.open(MAIN_CACHE)
+        await cache.addAll([
+            '.',
+            './favicon.ico',
+            './index.html',
+            './manifest.webmanifest',
+            './fmusic.jpg',
+            './081a2a07-7c5b-4c2f-a883-bcbd9c548a02.mp3',
+            './icons/icon_64.png',
+            './icons/icon_180.png',
+            './icons/icon_256.png',
+            './icons/icon_512.png'
+        ])
+    })())
+});
+
+const deleteCache = async (key) => {
+    await caches.delete(key);
+};
+
+const deleteOldCaches = async () => {
+    const cacheKeepList = [MAIN_CACHE];
+    const keyList = await caches.keys();
+    const cachesToDelete = keyList.filter((key) => !cacheKeepList.includes(key));
+    await Promise.all(cachesToDelete.map(deleteCache));
+};
+
+self.addEventListener("activate", (event) => {
+    event.waitUntil(deleteOldCaches());
+});
+
+self.addEventListener('fetch', function(event) {
+    if (event.request.headers.get('range')) {
+        var pos =
+        Number(/^bytes\=(\d+)\-$/g.exec(event.request.headers.get('range'))[1]);
+        event.respondWith(
+            caches.open(MAIN_CACHE)
+            .then(function(cache) {
+                return cache.match(event.request.url);
+            }).then(function(res) {
+                if (!res) {
+                    return fetch(event.request)
+                    .then(res => {
+                        return res.arrayBuffer();
+                    });
+                }
+                return res.arrayBuffer();
+            }).then(function(ab) {
+                return new Response(
+                ab.slice(pos),
+                {
+                    status: 206,
+                    statusText: 'Partial Content',
+                    headers: [
+                    ['Content-Range', 'bytes ' + pos + '-' +
+                        (ab.byteLength - 1) + '/' + ab.byteLength]]
+                });
+        }));
+    } else {
+        event.respondWith(
+            caches.match(event.request).then(function(response) {
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request).then(function(response) {
+                    return response;
+                }).catch(function(error) {
+                    throw error;
+                });
+            })
+        );
+    }
+});
