00947a05d334bff33a1b37af283238d499eed53e
corone <fxfx_pot@yahoo.co.jp>
2025-05-22 20:38:35 +0900

    change music

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 
-const MAIN_CACHE = 'main_20250519_2';
+const MAIN_CACHE = 'main_20250522';
 
 self.addEventListener("install", async (event) => {
     event.waitUntil((async () => {
@@ -10,9 +10,11 @@ self.addEventListener("install", async (event) => {
             './index.html',
             './manifest.webmanifest',
             './fmusic.jpg',
-            './081a2a07-7c5b-4c2f-a883-bcbd9c548a02.mp3',
+            // './081a2a07-7c5b-4c2f-a883-bcbd9c548a02.mp3',
+            './ea3824a9-7334-4a7b-879a-71e54390d015.mp3',
+            './1ba93da6-dada-4880-8694-00f124080d32.mp3',
             // './17c6eaa2-e8a0-4dbd-ba7c-d19c39c363a1.mp3',
-            './e382c0be-90e8-489c-8aa1-e61518ce971f.mp3',
+            // './e382c0be-90e8-489c-8aa1-e61518ce971f.mp3',
             // './c6413c20-b45e-467a-ac7b-264cf2ed5bfa.mp3',
             './icons/icon_64.png',
             './icons/icon_180.png',
